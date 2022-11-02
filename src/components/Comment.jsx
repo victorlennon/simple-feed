528f15168c5df9a71ad6987f0c5f0cd1b5290747
Victor Lennon <victorlennonfb@gmail.com>
2022-11-02 13:38:57 -0300

    add comments structure

---
+++ src/components/Comment.jsx
@@ -0,0 +1,32 @@
+import style from './Comment.module.css'
+import { ThumbsUp, Trash } from 'phosphor-react'
+
+export function Comment() {
+  return (
+    <div className={style.comment}>
+      <img src="https://github.com/victorlennon.png" alt="" />
+      <div className={style.commentBox}>
+        <div className={style.commentContent}>
+          <header>
+            <div className={style.authorAndTime}>
+              <strong>Victor Lennon</strong>
+              <time title="24 de outubro às 22:41" dateTime="2022-10-24 22:41:00">
+                Cerca de 1h atrás
+              </time>
+            </div>
+            <button title="Apagar comentário">
+              <Trash size={24} />
+            </button>
+          </header>
+          <p>Muito bom! Parabéns 👏👏👏</p>
+        </div>
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  )
+}
